800acdd633dec2a452a01bc9c93e5d061acc1a30
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-05-01 20:30:02 +0200

    Don't match root slash

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,8 @@ export NEXT_PUBLIC_API_URL=$BASE_URL_API
 # Replace http(s) with ws
 export NEXT_PUBLIC_WS_URL=$(sed -r "s|https?|ws|" <<< "$BASE_URL_API/ws")
 
-# Extract base path && remove trailing slash
-API_PATH=$(sed -E 's~.*//[^/]+/?(.*)~\1~' <<< $BASE_URL_API)
+# Extract base path & remove trailing slash
+API_PATH=$(sed -E 's~^[^/]+//[^/]+(/[^/?#][^?#]*)?.*~\1~' <<< $BASE_URL_API)
 export API_PATH=$(sed -e 's~/$~~' <<< $API_PATH)
 
 # Set ports
