3b33403264e3d9da9c001dc6d14d291b968b863b
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-05-01 03:49:21 +0200

    Construct api urls at start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,17 @@
 
 cd /home/bun/app
 
-echo "Running with api url ${NEXT_PUBLIC_API_URL}"
+# Set API and WebSocket URLs only if they aren't already set
+if [ ! -z "$BASE_URL" ]; then
+    export NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL:-"http://${BASE_URL}:${API_PORT}"}
+    export NEXT_PUBLIC_WS_URL=${NEXT_PUBLIC_WS_URL:-"ws://${BASE_URL}:${API_PORT}/ws"}
+    echo "Using API URL: ${NEXT_PUBLIC_API_URL}"
+    echo "Using WebSocket URL: ${NEXT_PUBLIC_WS_URL}"
+else
+    # Set some default values at least
+    export NEXT_PUBLIC_API_URL="http://localhost:${API_PORT}"
+    export NEXT_PUBLIC_WS_URL="ws://localhost:${API_PORT}/ws"
+fi
 
 # Set ports
 sed -i 's|port: 8080|port: ${API_PORT}|' ./apps/server/src/index.ts
